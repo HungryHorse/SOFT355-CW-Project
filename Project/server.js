3d63ed32ba9c9e055f1640b0e573782936e9f25b
Jack Brewer <jack.brewer@students.plymouth.ac.uk>
2020-01-05 12:31:38 +0000

    Added sockets

--- Project/server.js
+++ Project/server.js
@@ -1,15 +1,67 @@
 var http = require('http');
-
+var express = require('express');
 var finalhandler = require('finalhandler');
 var serveStatic = require('serve-static');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io').listen(server);
+
+var players = {};
 
 var serve = serveStatic("./");
+var playerNumber = 1;
 
-var server = http.createServer(function(req, res) {
-  var done = finalhandler(req, res);
-  serve(req, res, done);
+
+app.use(express.static('./'));//Serving static file
+
+io.on('connection', function (socket) {
+  connect(socket);
+  socket.on('disconnect', function () {
+    disconnect(socket);
+  });
 });
 
 server.listen(9000, function() {
-  console.log("Listening on " + 9000);
+    console.log("Server running at: http://localhost:" + 9000)
 });
+
+function connect(socket)
+{
+    if(playerNumber <= 4)
+    {
+        console.log("Player " + playerNumber + " has connected.");
+        players[socket.id] =
+        {
+            playerID: playerNumber
+        }
+        playerNumber++;
+    }
+    else
+    {
+        console.log("connection failed too many players");
+    }
+}
+
+function disconnect(socket)
+{
+    if(players[socket.id] != null)
+    {
+        if(playerNumber > 2)
+        {
+            console.log("Player " + players[socket.id].playerID + " has disconnected.");
+            delete players[socket.id];
+            playerNumber--;
+        }
+        else
+        {
+            console.log("Last player has disconnected.");
+            delete players[socket.id];
+            playerNumber--;
+        }
+    }
+}
+
+function playerDeath(playerNumber)
+{
+    console.log("Player " + playerNumber + " has died");
+}
