f17b60d34763ff1963d99f4b5324804cb47e886c
Jack Brewer <jack.brewer@students.plymouth.ac.uk>
2020-01-05 21:54:54 +0000

    4 Players

--- Project/server.js
+++ Project/server.js
@@ -7,9 +7,12 @@ var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
 
 var players = {};
+var obstacles = {};
+var destroyers = {};
 
 var serve = serveStatic("./");
 var playerNumber = 1;
+var playerInWinState = 0;
 
 
 app.use(express.static('./'));//Serving static file
@@ -18,6 +21,22 @@ io.on('connection', function (socket) {
   connect(socket);
   socket.on('disconnect', function () {
     disconnect(socket);
+  });
+
+  // when a player moves, update the player data
+  socket.on('playerMovement', function (movementData) {
+    players[socket.id].playerX = movementData.playerX;
+    players[socket.id].playerY = movementData.playerY;
+    // emit a message to all players about the player that moved
+    socket.broadcast.emit('playerMoved', players[socket.id]);
+  });
+
+  socket.on('winState', function (winStateObject)
+  {
+      players[socket.id].winState = winStateObject.playerWinState;
+      playerInWinState++;
+      checkForRoundEnd();
+      socket.broadcast.emit('winStateUpdated', players[socket.id]);
   });
 });
 
@@ -32,9 +51,18 @@ function connect(socket)
         console.log("Player " + playerNumber + " has connected.");
         players[socket.id] =
         {
-            playerID: playerNumber
+            socketID: socket.id,
+            playerX: 100,
+            playerY: 450,
+            winState: 'none'
         }
         playerNumber++;
+        // send the players object to the new player
+        socket.emit('currentPlayers', players);
+        // update all other players of the new player
+        socket.broadcast.emit('newPlayer', players[socket.id]);
+
+        checkForGameStart(socket);
     }
     else
     {
@@ -48,7 +76,7 @@ function disconnect(socket)
     {
         if(playerNumber > 2)
         {
-            console.log("Player " + players[socket.id].playerID + " has disconnected.");
+            console.log("A player has disconnected.");
             delete players[socket.id];
             playerNumber--;
         }
@@ -58,10 +86,47 @@ function disconnect(socket)
             delete players[socket.id];
             playerNumber--;
         }
+        io.emit('disconnect', socket.id);
     }
 }
 
-function playerDeath(playerNumber)
+function checkForGameStart(socket)
 {
-    console.log("Player " + playerNumber + " has died");
+    var gameStart = true;
+
+    if(playerNumber <= 4)
+    {
+        gameStart = false
+    }
+
+    if(gameStart)
+    {
+        startGame();
+    }
+}
+
+function checkForRoundEnd()
+{
+    var gameEnd = true;
+    if(playerInWinState < 4)
+    {
+        gameEnd = false;
+    }
+
+    if(gameEnd)
+    {
+        endRound();
+    }
+}
+
+function startGame()
+{
+    console.log("Game Start");
+    io.emit('gameStart');
+}
+
+function endRound()
+{
+    console.log("End Round");
+    playerInWinState = 0;
 }
